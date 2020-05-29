17d59c75dd6ecfe25825a67cd39e1fbaff13481e
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-05-29 18:03:35 +0100

    Initial commit

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    private Plane plane;
+
+    @Before
+    public void setUp(){
+        plane = new Plane(PlaneType.BOEING);
+    }
+    @Test
+    public void canGetPlaneType(){
+        assertEquals(PlaneType.BOEING, plane.getPlaneType() );
+    }
+    @Test
+    public void canGetPlaneCapacity(){
+        assertEquals(300, plane.getPlaneType().getCapacity());
+    }
+    @Test
+    public void canGetPlaneTotalWeight(){
+        assertEquals(5000, plane.getPlaneType().getTotalWeight());
+    }
+
 }
